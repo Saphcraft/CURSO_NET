b86e7e4b8ade68078fffac16a8ce52ae33faa7fb
email <mail@mail.com>
2021-07-16 20:12:26 +0200

    cambios de ejercicio

---
+++ EjercicioBancoConsola/EjercicioBancoConsola/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioBancoConsola
+{
+    class Program
+    {//prueba ejercicio
+        static void Main(string[] args)
+        {
+        }
+    }
+}
