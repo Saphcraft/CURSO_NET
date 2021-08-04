a5eb12250123f8755ded0feae3f22ba5b590140a
email <mail@mail.com>
2021-08-04 20:24:59 +0200

    Aplicacion Consola clases

---
+++ consolaAppPRODUCTOS/consolaAppPRODUCTOS/Producto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolaAppPRODUCTOS
+{
+    class Producto
+    {
+        //atributos
+        private string nombre, categoria;
+        private double precio;
+        //propiedades
+        public string PNombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string PCategoria
+        {
+            get { return categoria; }
+            set { categoria = value; }
+        }
+        public double PPrecio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+        //constructores
+        public Producto() { }
+        //constructor de 3 parametros
+        public Producto(string c_nombre, double c_precio, string c_categoria)
+        {
+            this.nombre = c_nombre;
+            this.precio = c_precio;
+            this.categoria = c_categoria;
+        }
+        //constructor de 2 parametros
+        public Producto(string c_nombre, double c_precio)
+        {
+            this.nombre = c_nombre;
+            this.precio = c_precio;
+        }
+        //metodos
+        public void pedido()
+        {
+
+        }
+    }
+}
---
+++ consolaAppPRODUCTOS/consolaAppPRODUCTOS/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolaAppPRODUCTOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //crear el objeto producto
+            Producto nuevoProduct = new Producto();
+
+            //Crear variables para recibir cadenas
+            string c1 = "", c2 = "", eleccion = "";
+            double n1 = 0;
+            //crear menu con crear objeto, cambiar datos, listar datos y salir
+            do
+            {
+                Console.WriteLine("Pulse:\n1. CREARORBJETO\n2. HACER PEDIDO\n3. MOSTRAR EL TOTAL + IVA\n4. SALIR");
+                eleccion = Console.ReadLine();
+                Console.Clear();
+                switch (eleccion)
+                {
+                    //el usuario introduce los datos correspondientes para crear
+                    case "1":
+                        Console.WriteLine("Escribe el nombre");
+                        c1 = Console.ReadLine();
+                        Console.WriteLine("Escribe el precio");
+                        n1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Escribe la categoria");
+                        c2 = Console.ReadLine();
+                        //usa el constructor de 2 parametros
+                        nuevoProduct = new Producto(c1, n1);
+                        //usa el constructor de 3 parametros
+                        nuevoProduct = new Producto(c1, n1, c2);
+                        break;
+                    //el usuario hace el pedido
+                    case "2":
+
+
+                        Console.Clear();
+                       
+                    //invocamos el metodo de la clase para listar
+                    case "3":
+                        mp.Imprimir();
+                        break;
+                    //salir de la aplicacion
+                    case "4":
+                        Console.WriteLine("ADIOS");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("has introducido un valor incorrecto");
+                        break;
+                }
+            } while (eleccion != "4");
+        }
+    }
+}
